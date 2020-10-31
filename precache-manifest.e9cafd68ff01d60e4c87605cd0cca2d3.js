8cacc5cd76a0a0418361c9e26135779b43654b82
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-31 15:28:40 +0000

    Deploy v2rayA/v2raya-web to github.com/v2rayA/v2raya-web.git:master

---
+++ precache-manifest.e9cafd68ff01d60e4c87605cd0cca2d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fcc3a98e8ae4afc3b5c80635c88ad5",
+    "url": "CNAME"
+  },
+  {
+    "revision": "ab27bf358699834a12ac",
+    "url": "css/app.7d894567.css"
+  },
+  {
+    "revision": "378a2d1f28060a4eddbf",
+    "url": "css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "75123a50ccd53bda0f2ebe2de5427f06",
+    "url": "fonts/iconfont.75123a50.ttf"
+  },
+  {
+    "revision": "8792b78f7b9e2bfe546f656dbf75e33f",
+    "url": "fonts/iconfont.8792b78f.woff2"
+  },
+  {
+    "revision": "9979d3b4f676674cd8bfe96c858f73b9",
+    "url": "fonts/iconfont.9979d3b4.woff"
+  },
+  {
+    "revision": "eb74af6f48671b1da5778aed23acb94f",
+    "url": "fonts/iconfont.eb74af6f.eot"
+  },
+  {
+    "revision": "50162f60f2cd5ba81c94bbd8ec32dee0",
+    "url": "img/flags/flag_en.svg"
+  },
+  {
+    "revision": "3a5b5ac1ac98c76572f65c1ddd6dcadf",
+    "url": "img/flags/flag_zh.svg"
+  },
+  {
+    "revision": "5ce46c1262680b286187e30fe11d5393",
+    "url": "img/iconfont.5ce46c12.svg"
+  },
+  {
+    "revision": "5fd6eecaa53e183b0f378fab82e7d108",
+    "url": "img/logo2.5fd6eeca.png"
+  },
+  {
+    "revision": "c07ac3932dbd8d5a87580f784759b73a",
+    "url": "index.html"
+  },
+  {
+    "revision": "ab27bf358699834a12ac",
+    "url": "js/app.68c0515f.js"
+  },
+  {
+    "revision": "378a2d1f28060a4eddbf",
+    "url": "js/chunk-vendors.be15420e.js"
+  },
+  {
+    "revision": "90b5848ee73327b849a31c62c7b41dc6",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "robots.txt"
+  }
+]);
